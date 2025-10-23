f6cf65df60b9d99b70c6df3ea73877da84f5392e
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-10-23 12:29:05 +0900

    Removed comment out line

--- Example/Example/ContentView.swift
+++ Example/Example/ContentView.swift
@@ -13,7 +13,6 @@ struct ContentView: View {
 
     var body: some View {
         Calculator(value: $value)
-//            .calculatorStyle(.classic(buttonBorderShape: .roundedRectangle))
     }
 }
 
