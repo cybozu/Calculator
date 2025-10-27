7e5b0141e51e3950cc7097181a66552a29315d7f
Takuto NAKAMURA (Kyome) <kyomesuke@gmail.com>
2025-10-27 17:29:05 +0900

    Document calculatorStyle

--- Sources/Calculator/Extensions/View+Extension.swift
+++ Sources/Calculator/Extensions/View+Extension.swift
@@ -1,6 +1,14 @@
 import SwiftUI
 
 extension View {
+    /// Sets the style for calculator view within this view.
+    ///
+    /// Use this modifier to set a specific style for calculator view instances within a view:
+    ///
+    /// ```swift
+    /// Calculator()
+    ///     .calculatorStyle(.classic())
+    /// ```
     public func calculatorStyle(_ style: some CalculatorStyle) -> some View {
         environment(\.calculatorStyle, style)
     }
